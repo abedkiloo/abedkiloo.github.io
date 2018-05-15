531c2b7c2bf650ba69c3f221bf86d73899349af7
Abednego Wambua <abedxh@gmail.com>
2018-05-15 19:37:14 +0300

    first commit

---
+++ src/app/data.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+
+  private goals = new BehaviorSubject<any>(['Initial Goal', 'Own an RVR']);
+  goal = this.goals.asObservable();
+
+  constructor() {
+  }
+
+  changeGoal(goal) {
+    this.goals.next(goal);
+  }
+}
